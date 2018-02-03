fcf3d4ddc47d8240f1a4f31567aec6d6cb2a642f
Michael Gutman <michaelgutman@hotmail.com>
2018-02-03 00:54:08 -0500

    add signup page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
   return render_template('home.html')
 
+@app.route("/showSignUp")
+def showSignUp():
+	return render_template('signup.html')
+
 if __name__ == "__main__":
   app.run()
