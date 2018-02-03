9841c2083deec135ebaa45f0ea5ced1da69cd244
Michael Gutman <michaelgutman@hotmail.com>
2018-02-03 01:17:51 -0500

    added basic signup fn

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,17 @@ def main():
 @app.route("/showSignUp")
 def showSignUp():
 	return render_template('signup.html')
+
+@app.route("/signUp", methods=['POST'])
+def signUp():
+	_name = request.form['inputName']
+	_email = request.form['inputEmail']
+	_password = request.form['inputPassword']
+
+	if _name and _email and _password:
+		return json.dumps({'html':'<span>Thanks for signing up!</span>'})
+	else:
+		return json.dumps({'html':'<span>Please fill in all the fields</span>'})
 
 if __name__ == "__main__":
   app.run()
